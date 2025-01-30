68f4d90b112065563c22933cd91aaf1a465f9ab9
alissaryb <alissaryb@gmail.com>
2025-01-30 17:39:33 +0300

    2

--- main.py
+++ main.py
@@ -11,11 +11,8 @@ from forms.courses import FormAddCourse
 import json
 import datetime
 
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
-
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -30,7 +27,6 @@ def practice():
             a = json.load(json_file)
     except ValueError:
         a = {}
-
 
     tasks = []
     for i in range(len(a)):
@@ -83,55 +79,68 @@ def work():
         with open("answers_users.json", 'w') as json_file:
             json.dump(cur_ans_user, json_file)
 
-        return render_template("work.html", title="", tasks=tasks, is_check=True, form=form, users_answers=users_answers, mark=mark, max_mark=len(tasks))
+        return render_template("work.html", title="", tasks=tasks,
+                               is_check=True, form=form,
+                               users_answers=users_answers, mark=mark,
+                               max_mark=len(tasks))
 
-    return render_template("work.html", title="", tasks=tasks, is_check=False, form=form, users_answers=users_answers)
-
+    return render_template("work.html", title="", tasks=tasks, is_check=False,
+                           form=form, users_answers=users_answers)
 
 
 @app.route('/all_courses', methods=['GET', 'POST'])
 def all_courses():
-    courses = [{"id": "0", "cours_name": "Подготовка к ЕГЭ на Пупоне",
-                "subject": "Инфа", "type": "video",
-                "link": "https://rutube.ru/play/embed/9852193",
-                "description": "Господи, это такой крутой курс. Вам он жизнено необходим. Я готов перестать есть сникерсы, ради этого курса"},
-               {"id": "1", "cours_name": "HHHHHHHHHHHHH", "subject": "Русич",
-                "type": "presentation",
-                "link": "https://nsportal.ru/sites/default/files/2023/01/05/podgotovka_k_ege_2023_no1.pptx",
-                "description": "Господи, это такой крутой курс."},
-               {"id": "2", "cours_name": "UUUUUUUUUUUU", "subject": "Матан",
-                "description": "Господи, это такой крутой курс."},
-               {"id": "3", "cours_name": "IIIIIIIIII", "subject": "Физ",
-                "description": "Господи, это такой крутой курс."}]
+    courses = [
+        {"uuid": "0", "title": "Подготовка к ЕГЭ на Пупоне", "subject": "Инфа",
+         "type": "public",
+         "description": "Господи, это такой крутой курс. Вам он жизнено необходим. Я готов перестать есть сникерсы, ради этого курса",
+         "token": "36b4b99", "made_on_datetime": 2424, "author": "ЕlnjnlnlС"},
+        {"uuid": "1", "title": "Подготовка к ЕГЭ на Пупоне", "subject": "Инфа",
+         "type": "public",
+         "description": "Господи, это такой крутой курс. Вам он жизнено необходим. Я готов перестать есть сникерсы, ради этого курса",
+         "token": "36b4b99", "made_on_datetime": 2424, "author": "Фёдоров Кирилл Евгеньевич"},
+        {"uuid": "2", "title": "Подготовка к ЕГЭ на Пупоне", "subject": "Инфа",
+         "type": "public",
+         "description": "Господи, это такой крутой курс. Вам он жизнено необходим. Я готов перестать есть сникерсы, ради этого курса",
+         "token": "36b4b99", "made_on_datetime": 2424, "author": "ЕmmjnjlnljС"}]
 
     return render_template("all_courses.html", title="", courses=courses)
 
+
 @app.route('/course/<id>', methods=['GET', 'POST'])
 def courses(id):
-    courses = [{"id": "0", "cours_name": "Подготовка к ЕГЭ на Пупоне",
-                "subject": "Инфа", "type": "video", "link": "https://rutube.ru/play/embed/9852193",
-                "description": "Господи, это такой крутой курс. Вам он жизнено необходим. Я готов перестать есть сникерсы, ради этого курса"},
-               {"id": "1", "cours_name": "HHHHHHHHHHHHH", "subject": "Русич",
-                "type": "presentation", "link": "https://nsportal.ru/sites/default/files/2023/01/05/podgotovka_k_ege_2023_no1.pptx",
-                "description": "Господи, это такой крутой курс."},
-               {"id": "2", "cours_name": "UUUUUUUUUUUU", "subject": "Матан",
-                "description": "Господи, это такой крутой курс."},
-               {"id": "3", "cours_name": "IIIIIIIIII", "subject": "Физ",
-                "description": "Господи, это такой крутой курс."}]
+    courses = [{"uuid": "0", "title": "Подготовка к ЕГЭ на Пупоне", "subject": "Инфа",
+         "type": "public",
+         "description": "Господи, это такой крутой курс. Вам он жизнено необходим. Я готов перестать есть сникерсы, ради этого курса",
+         "token": "36b4b99", "made_on_datetime": 2424, "author": "ЕlnjnlnlС"},
+        {"uuid": "1", "title": "Подготовка к ЕГЭ на Пупоне", "subject": "Инфа",
+         "type": "public",
+         "description": "Господи, это такой крутой курс. Вам он жизнено необходим. Я готов перестать есть сникерсы, ради этого курса",
+         "token": "36b4b99", "made_on_datetime": 2424, "author": "Фёдоров Кирилл Евгеньевич"},
+        {"uuid": "2", "title": "Подготовка к ЕГЭ на Пупоне", "subject": "Инфа",
+         "type": "public",
+         "description": "Господи, это такой крутой курс. Вам он жизнено необходим. Я готов перестать есть сникерсы, ради этого курса",
+         "token": "36b4b99", "made_on_datetime": 2424, "author": "ЕmmjnjlnljС"}]
 
     course1 = courses[int(id)]
     print(id, course1['id'])
+    publications = [{"uuid": "0", "title": "Кейс 2", "text": "Домашка по предпрофу, вы умрете, вы умрете, вы умрете, вы умрете, вы умрете, вы умрете", "made_on_datetime": "432872", "files_folder_path": [(), ()], "type": "courses", "author": "Ф К Е"}, {}, {}]
     return render_template("course.html", title="", id=id, course1=course1)
-
-
 
 
 @app.route('/profile', methods=['GET', 'POST'])
 @app.route('/profile/<name>', methods=['GET', 'POST'])
 def profile(name=""):
-    user_group = [{"token": "g4d65g", "group_name": "11 класс it"}, {"token": "р375gy", "group_name": "10 класс mt"}]
+    user_group = [{"token": "g4d65g", "group_name": "11 класс it"},
+                  {"token": "р375gy", "group_name": "10 класс mt"}]
 
-    return render_template("profile.html", title="Личный кабинет", user={'id': '0', 'name': 'Алиса', 'surname': 'Рыбакова', 'lastname': 'Рыбакова', 'email': 'a1@a.com', 'phone_number': '89154559579', 'password': 'qwerty123', 'class_num': 10 }, user_group=user_group, len=len(user_group))
+    return render_template("profile.html", title="Личный кабинет",
+                           user={'id': '0', 'name': 'Алиса',
+                                 'surname': 'Рыбакова', 'lastname': 'Рыбакова',
+                                 'email': 'a1@a.com',
+                                 'phone_number': '89154559579',
+                                 'password': 'qwerty123', 'class_num': 10},
+                           user_group=user_group, len=len(user_group))
     referer = request.base_url
 
     if referer == "http://127.0.0.1:8080/profile":
@@ -152,7 +161,9 @@ def profile(name=""):
             print(user)
 
             user_group = []
-            return render_template("profile.html", title="Личный кабинет", user=user, user_group=user_group, len=len(user_group))
+            return render_template("profile.html", title="Личный кабинет",
+                                   user=user, user_group=user_group,
+                                   len=len(user_group))
 
     return render_template("profile.html", title="Личный кабинет",
                            user={'id': '0', 'name': 'Алиса',
@@ -160,12 +171,15 @@ def profile(name=""):
                                  'email': 'a1@a.com',
                                  'phone_number': '89154559579',
                                  'password': 'qwerty123', 'class_num': 10})
-                           #user_group=user_group, len=len(user_group))
+    # user_group=user_group, len=len(user_group))
+
 
 @app.route('/statistic', methods=['GET', 'POST'])
 def statistic():
-    arr = {1: {"correct": 10, "all": 23}, 2: {"correct": 15, "all": 15}, 3: {"correct": 1, "all": 20}, 4: {"correct": 34, "all": 100}, 5: {"correct": 22, "all": 37}, 6: {"correct": 16, "all": 56}}
-    d = [i for i in range(1, len(arr)+1)]
+    arr = {1: {"correct": 10, "all": 23}, 2: {"correct": 15, "all": 15},
+           3: {"correct": 1, "all": 20}, 4: {"correct": 34, "all": 100},
+           5: {"correct": 22, "all": 37}, 6: {"correct": 16, "all": 56}}
+    d = [i for i in range(1, len(arr) + 1)]
 
     for i in d:
         p = arr[i]['correct'] / arr[i]['all'] * 100
@@ -173,16 +187,16 @@ def statistic():
 
     return render_template("statistic.html", arr=arr, d=d)
 
+
 @app.route('/add_course', methods=['GET', 'POST'])
 def add_course():
     form = FormAddCourse()
     if form.submit.data == True:
-
         print(form.type.data)
 
         return redirect("/")
-    return render_template("add_course.html", title="Добавление курса", form=form)
-
+    return render_template("add_course.html", title="Добавление курса",
+                           form=form)
 
 
 @app.route('/add_task', methods=['GET', 'POST'])
@@ -217,7 +231,8 @@ def add_task():
             f.write(form.task.data)
 
         return redirect("/")
-    return render_template("add_task.html", title="Добавление задания", form=form)
+    return render_template("add_task.html", title="Добавление задания",
+                           form=form)
 
 
 @app.route('/teacher_groups', methods=['GET', 'POST'])
@@ -240,7 +255,6 @@ def register():
             a = {}
 
         id = str(len(a))
-
 
         user = {
             "id": id,
@@ -277,21 +291,25 @@ def login():
         cur_user = form.email.data
         cur_pas = form.password.data
         for i in range(len(a)):
-            if a[str(i)]['email'] == cur_user or a[str(i)]['email'] == cur_user:
+            if a[str(i)]['email'] == cur_user or a[str(i)][
+                'email'] == cur_user:
                 if a[str(i)]['password'] == cur_pas:
                     return redirect("/profile/" + a[str(i)]['nickname'])
                 else:
-                    return render_template("login.html", title="Авторизация", form=form, message="Неверный пароль")
+                    return render_template("login.html", title="Авторизация",
+                                           form=form,
+                                           message="Неверный пароль")
 
-        return render_template("login.html", title="Авторизация", form=form, message="Такого пользователя не существует")
-
+        return render_template("login.html", title="Авторизация", form=form,
+                               message="Такого пользователя не существует")
 
     return render_template("login.html", title="Авторизация", form=form)
 
 
 @app.route('/error1', methods=['GET', 'POST'])
 def error1():
-    return render_template("error.html", err="Такого пользователя не существует")
+    return render_template("error.html",
+                           err="Такого пользователя не существует")
 
 
 if __name__ == "__main__":
