08cabc498cd638ea3b1958827d837c0a82883593
NikitaMulyar <nickm.school@mail.ru>
2025-02-01 14:30:19 +0300

    models

---
+++ sa_models/problems.py
@@ -0,0 +1,22 @@
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class Problem(SqlAlchemyBase):
+    __tablename__ = 'problems'
+    uuid = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
+    text = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
+    files_folder_path = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    source = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    answer = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+    kim_type_uuid = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey("kim_types.uuid"))
+    kim_type = orm.relationship("User")
+
+    difficulty_uuid = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey("difficulties.uuid"))
+    difficulty = orm.relationship("User")
+
+    def __repr__(self):
+        return f'<Problem> {self.uuid} {self.text}'
